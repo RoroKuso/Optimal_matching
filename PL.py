e0519122a00a5c3b12e6ea420c43c248096f0217
Rodrigue T <tavernier.rodrigue@gmail.com>
2022-11-23 23:08:36 +0100

    useless ecart type data for question_1

--- PL.py
+++ PL.py
@@ -11,7 +11,8 @@ def question_1(lower: int, upper: int, average_iter: int = 10, size_lim: int = 1
     for N in range(step, size_lim+1, step):
         logging.info("---->Starting N = {}".format(N))
         start = time.time()
-        # average of ``average_iter`` iterations
+        #| Average of ``average_iter`` iterations
+        total_satisfaction = np.zeros(N)
         for i in range(average_iter):
             with gp.Env(empty=True) as env:
                 
@@ -24,6 +25,8 @@ def question_1(lower: int, upper: int, average_iter: int = 10, size_lim: int = 1
                     m.addConstrs(gp.quicksum(X[j]) == 1 for j in range(N))
                     m.addConstrs(gp.quicksum(X.transpose()[j]) == 1 for j in range(N))
                     m.optimize(callback=None)
+                    total_satisfaction += [U[i][np.argmax(X[i])] for i in range(N)]
+        logging.info("    | Ecart type satisfaction : {}".format(np.nanstd(total_satisfaction)))
         elapsed = (time.time() - start)/average_iter
         logging.info(f"    | Average time = {elapsed:.4f} s")
     logging.info("Finished tests for question_1")
